41695b7446ae35c61eb8e3c8a4480b8c17eb9e79
luciafonsecadesign <luciafonsecadesign@gmail.com>
2022-10-14 19:55:17 -0500

    Modificaciones

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-sala-de-estar",
-      "name": "Sala de estar",
+      "id": "0-entrada",
+      "name": "Entrada",
       "levels": [
         {
           "tileSize": 256,
@@ -30,8 +30,8 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.07977130589595127,
-          "pitch": 0.23883928015142253,
+          "yaw": 0.27747152963498145,
+          "pitch": 0.3039282066556428,
           "rotation": 0,
           "target": "1-puestos-de-trabajo"
         }
@@ -68,22 +68,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.197253582140533,
-          "pitch": 0.3792005297479619,
+          "yaw": 0.333537362278312,
+          "pitch": 0.36231486967028204,
           "rotation": 0,
           "target": "2-plotter"
         },
         {
-          "yaw": 0.9734424184264583,
-          "pitch": 0.2139949075306884,
+          "yaw": 2.9892751078296964,
+          "pitch": 0.1778750157478619,
           "rotation": 0.7853981633974483,
-          "target": "5-almacn"
-        },
-        {
-          "yaw": 3.055889206633948,
-          "pitch": 0.23954948621523542,
-          "rotation": 0,
-          "target": "0-sala-de-estar"
+          "target": "0-entrada"
         }
       ],
       "infoHotspots": []
@@ -118,72 +112,22 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": 0.1324548348775334,
-          "pitch": 0.22503986865231518,
-          "rotation": 0,
-          "target": "3-oficina-contabilidad"
+          "yaw": 0.1332240690445019,
+          "pitch": 0.08353050639744986,
+          "rotation": 0.7853981633974483,
+          "target": "4-oficina-auxiliar"
         },
         {
-          "yaw": 1.781998179952244,
-          "pitch": 0.23341375047110624,
+          "yaw": -0.20345775351293582,
+          "pitch": 0.0795041689188487,
           "rotation": 5.497787143782138,
-          "target": "5-almacn"
-        },
-        {
-          "yaw": -0.22696261709327992,
-          "pitch": 0.21307859115255035,
-          "rotation": 0,
-          "target": "4-oficina-principal"
-        },
-        {
-          "yaw": 2.8614216040942093,
-          "pitch": 0.4329861376611266,
-          "rotation": 0,
-          "target": "1-puestos-de-trabajo"
+          "target": "3-oficina-principal"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-oficina-contabilidad",
-      "name": "Oficina contabilidad",
-      "levels": [
-        {
-          "tileSize": 256,
-          "size": 256,
-          "fallbackOnly": true
-        },
-        {
-          "tileSize": 512,
-          "size": 512
-        },
-        {
-          "tileSize": 512,
-          "size": 1024
-        },
-        {
-          "tileSize": 512,
-          "size": 2048
-        }
-      ],
-      "faceSize": 2048,
-      "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
-        "fov": 1.5707963267948966
-      },
-      "linkHotspots": [
-        {
-          "yaw": 0.12371748591404952,
-          "pitch": 0.2810324319662172,
-          "rotation": 0,
-          "target": "2-plotter"
-        }
-      ],
-      "infoHotspots": []
-    },
-    {
-      "id": "4-oficina-principal",
+      "id": "3-oficina-principal",
       "name": "Oficina Principal",
       "levels": [
         {
@@ -212,17 +156,17 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -0.031078126667019035,
-          "pitch": 0.3073329341484481,
-          "rotation": 0,
+          "yaw": -0.04659843553989873,
+          "pitch": 0.15837060453014828,
+          "rotation": 5.497787143782138,
           "target": "2-plotter"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-almacn",
-      "name": "Almacén",
+      "id": "4-oficina-auxiliar",
+      "name": "Oficina Auxiliar",
       "levels": [
         {
           "tileSize": 256,
@@ -250,16 +194,16 @@ var APP_DATA = {
       },
       "linkHotspots": [
         {
-          "yaw": -2.862443055091349,
-          "pitch": 0.1501765239153059,
-          "rotation": 13.351768777756625,
+          "yaw": 0.33679348224132255,
+          "pitch": 0.22559568427237053,
+          "rotation": 0,
           "target": "2-plotter"
         }
       ],
       "infoHotspots": []
     }
   ],
-  "name": "ALEJANDRO VELÁSQUEZ",
+  "name": "AVP DIMMEC S.A.S",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
